8a4b04321f4cd1579b940b65a58a70a478d8016e
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-24 10:39:15 -0400

    commit tree 6333cdc386b5f27e8193ad1638d156f12a4ac30e

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,5 +10,20 @@ class LinkedList
 
   def reverse!
     # your code here
+    return if @head.nil? || @head.next_node.nil?
+
+    current = @head
+    previous = nil
+
+    while current
+      next_node = current.next_node
+      current.next_node = previous
+      current.prev_node = next_node
+      previous = current
+      current = next_node
+    end
+
+    # Update head and tail references
+    @head, @tail = @tail, @head
   end
 end
